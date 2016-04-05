7482a4cdbeda6e983d1e2de8c66dd51701a899b4
Adam Riddell <riddelladam42@gmail.com>
2016-04-05 13:11:37 +1000

    Set up for phase 2

--- src/app.js
+++ src/app.js
@@ -5,4 +5,17 @@ intro.controller('introCtrl', ['$scope', function($scope) {
     $scope.red = 255;
     $scope.green = 255;
     $scope.blue = 255;
+
+    /**
+     * Generate a background colour object for use with ng-style.
+     *
+     * @param number red
+     * @param number green
+     * @param number blue
+     * @return object rgbColour
+     */
+    $scope.getRgbColour = function(red, green, blue) {
+        var rgb = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+        return {'background-color' : rgb};
+    };
 }]);
