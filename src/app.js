a9e3fdd512e28f0254f3c9162c7bc064f726cfc9
Adam Riddell <riddelladam42@gmail.com>
2016-04-05 13:20:00 +1000

    Set up for phase 3

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ intro.controller('introCtrl', ['$scope', function($scope) {
     $scope.red = 255;
     $scope.green = 255;
     $scope.blue = 255;
+    $scope.logoList = [];
 
     /**
      * Generate a background colour object for use with ng-style.
@@ -17,5 +18,16 @@ intro.controller('introCtrl', ['$scope', function($scope) {
     $scope.getRgbColour = function(red, green, blue) {
         var rgb = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
         return {'background-color' : rgb};
+    };
+
+    /**
+     * Add a coloured logo object to logoList.
+     *
+     * @param number red
+     * @param number green
+     * @param number blue
+     */
+    $scope.addLogo = function(red, green, blue) {
+        $scope.logoList.push({'red' : red, 'green' : green, 'blue' : blue});
     };
 }]);
